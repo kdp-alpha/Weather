d68ca040e2fc96eecfa798af3ee19d1fc6ae70ef
karan pawar <85121133+kdp-alpha@users.noreply.github.com>
2023-05-05 20:44:37 +0530

    connected frontend and backend

--- client/src/components/Page.jsx
+++ client/src/components/Page.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-
+import axios from 'axios'
+import { useState } from "react";
 const Page = () => {
-
-
-
+  const [city,setCity] = useState('');
+  const handleSubmit = async(e) => {
+      e.preventDefault();
+      const weatherData = await axios.post('/',{city})
+      setCity('');
+      console.log(weatherData.data)
+  }
   return (
     <div className="bg-sky-200" >
-      <form className="w-[100vw] h-[100vh] flex flex-col justify-center items-center">
+      <form onSubmit={handleSubmit} className="w-[100vw] h-[100vh] flex flex-col justify-center items-center">
       <input
         type="text"
         className="text-center w-1/2 border border-black rounded-md h-12"
         placeholder="Enter City Name"
-       
+        value={city}
+       onChange={e=>setCity(e.target.value)}
       />
       <button className="mt-9 bg-green-500 w-40 h-9 rounded-md">
         Search
